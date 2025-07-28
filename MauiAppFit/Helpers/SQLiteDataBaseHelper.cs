7d8db5678ff01757ca76e96c0f76b04d94e93ea6
231052 <231052@CPD02-M17>
2025-07-28 10:53:55 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiAppFit.Models;
+
+namespace MauiAppFit.Helpers
+{
+    public class SQLiteDataBaseHelper
+    {
+        readonly SQLiteAsyncConnection _db;
+        public SQLiteDataBaseHelper(string dbPath)
+        {
+            _db = new SQLiteAsyncConnection(dbPath);
+            _db.CreateTableAsync<Atividade>().Wait();
+        }
+        public Task<List<Atividade>> GetAllRows()
+        {
+
+        }
+    }
+}
---
+++ MauiAppFit/Models/Atividade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppFit.Models
+{
+    public class Atividade
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string? Descricao { get; set; }
+        public DataSetDateTime Data {  get; set; }
+        public double? Peso { get; set; }
+        public string? Observacoes { get; set; }
+    }
+}
